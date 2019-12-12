94c1f3475e5621e22adf073aa494796db413dfb1
20akumbhar <20akumbhar@gmail.com>
2019-12-12 13:55:41 +0530

    Text Ediror Added..

--- public/app.js
+++ public/app.js
@@ -12,4 +12,15 @@ $(".close").click(function(){
 
 $('#myModal').on('shown.bs.modal', function () {
     $('#myInput').trigger('focus')
-  })
+  });
+
+
+
+  function switchview(view){
+      $.get({
+          url:view,
+          cache: false
+      }).then(function(data){
+          $("#post-area").html(data);
+      });
+  }
